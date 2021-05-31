81cd9499c3bea96ad6b3f1bb048da49ed9151ed3
Eko Wijaya <ekowijayawindows@gmail.com>
2021-05-31 20:50:29 +0700

    fix bugs host value

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,18 +1,9 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-
-
-const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-          host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
-
-    });
-
-    server.route(routes);
-
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-};
-
-init();
+const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : ' 172.31.25.30',
+    routes: {
+        cors: {
+            origin: ['*'],
+        },
+    },
+});
